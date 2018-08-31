4d92f522e34b787a676e4893a6c9ed95a6b2d9f6
Hyago Procopio Brito <hyago-brito@hotmail.com>
2018-08-31 09:16:19 -0300

    Update Q684.py

--- Q684/Q684.py
+++ Q684/Q684.py
@@ -7,20 +7,22 @@ encomendas = encomendas.split()
 parametros = list(map(int, parametros))
 casas = list(map(int, casas))
 encomendas = list(map(int, encomendas))
-
-x = 0
+dicionario = {}
+i = 0
+while i < parametros[0]:
+  dicionario[casas[i]] = i
+  i += 1
 i = 0
 j = 0
 contador = 0
 while i < parametros[1]:
-  if (encomendas[i] < casas[j]):
-    while encomendas[i] != casas[j]:      
-      j -= 1
-      contador += 1
-  elif(encomendas[i] > casas[j]):
-    while encomendas[i] != casas[j]:
-      j += 1
-      contador += 1
+  if encomendas[i] != casas[j]:
+    x = dicionario[encomendas[i]]
+    k = j - x
+    j = x
+    if(k < 0):
+      k *= -1
+    contador += k
   i += 1
 
-print (contador)
+print(contador)
